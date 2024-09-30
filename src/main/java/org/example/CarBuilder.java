81cfa7b69ecb8889647e7ff6f5985f1c09302d34
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-09-30 09:56:28 +0200

    feat:add implemented the car builder

--- src/main/java/org/example/CarBuilder.java
+++ src/main/java/org/example/CarBuilder.java
@@ -1,4 +1,32 @@
 package org.example;
 
 public class CarBuilder {
+
+    private int year;
+    private String make;
+    private String model;
+
+    public static CarBuilder carBuilder() {
+        return new CarBuilder();
+    }
+
+    public CarBuilder setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public CarBuilder setMake(String make) {
+        this.make = make;
+        return this;
+    }
+
+    public CarBuilder setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public Car build() {
+        Car car = new Car(make,model,year);
+        return car;
+    }
 }
